fd0b735f554384a81f9904f36632b46a7d77491b
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-10-25 20:24:54 -0400

    react-a-Mole

---
+++ react-a-mole/src/App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+import MoleContainer from './Components/MoleContainer';
+import { useState } from 'react'
+import Mole from './Components/Mole';
+import EmptySlot from './Components/EmptySlot';
+
+
+function App(){
+    let [score, setScore] = useState(0)
+
+    const createMoleHill = () => {
+        let hills = []
+        for (let i = 0; i < 9; i++) {
+            hills.push(
+                <MoleContainer
+                key={i}
+                setScore={setScore}
+                score={score} />
+            )
+        }
+    
+        return (
+            <div>
+                {hills}
+            </div>
+        )
+    }
+
+    return (
+        <div className="App">
+            <h1>React-a-Mole!</h1>
+            {score}
+            {createMoleHill()}
+        </div>
+    )
+}
+
+export default App
+
